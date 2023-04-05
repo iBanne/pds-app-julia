53e441f53b2d49e0029f7e867d7b2e3b0cef8f32
ビクター <113388515+iBanne@users.noreply.github.com>
2023-04-04 22:12:41 -0400

    app-contato

---
+++ app-contato-winform-master/app-contato-winform-master/Listar.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AppContatoForm
+{
+    partial class Listar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListar = new System.Windows.Forms.DataGridView();
+            this.txtpesquisar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListar
+            // 
+            this.dgvListar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListar.Location = new System.Drawing.Point(50, 108);
+            this.dgvListar.Name = "dgvListar";
+            this.dgvListar.RowHeadersWidth = 62;
+            this.dgvListar.RowTemplate.Height = 28;
+            this.dgvListar.Size = new System.Drawing.Size(822, 426);
+            this.dgvListar.TabIndex = 0;
+            // 
+            // txtpesquisar
+            // 
+            this.txtpesquisar.Location = new System.Drawing.Point(50, 67);
+            this.txtpesquisar.Name = "txtpesquisar";
+            this.txtpesquisar.Size = new System.Drawing.Size(822, 26);
+            this.txtpesquisar.TabIndex = 2;
+            this.txtpesquisar.TextChanged += new System.EventHandler(this.txtpesquisar_TextChanged);
+            // 
+            // Listar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 578);
+            this.Controls.Add(this.txtpesquisar);
+            this.Controls.Add(this.dgvListar);
+            this.Name = "Listar";
+            this.Text = "Listar";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListar;
+        private System.Windows.Forms.TextBox txtpesquisar;
+    }
+}
---
+++ app-contato-winform-master/app-contato-winform-master/Listar.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppContatoForm.Properties;
+using MySql.Data.MySqlClient;
+
+namespace AppContatoForm
+{
+    public partial class Listar : Form
+    {
+        private MySqlConnection conexao;
+
+        private MySqlCommand comando;
+        public Listar()
+        {
+            InitializeComponent();
+            Conexao();
+            ListarContatos();
+        }
+        private void Conexao()
+        {
+            string conexaoString = "server=localhost;database=bd_pds;user=root;password=root;port=3306";
+            conexao = new MySqlConnection(conexaoString);
+            comando = conexao.CreateCommand();
+
+            conexao.Open();
+        }
+
+        private void ListarContatos()
+        {
+            var sql = "select * from contato_con;";
+            var cmd = new MySqlCommand(sql, conexao);
+            var da = new MySqlDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+            DataTable dados = new DataTable();
+            da.Fill(dados);
+            dgvListar.DataSource = dados;
+            dados.Columns["id_con"].ColumnName = "id";
+            dados.Columns["nome_con"].ColumnName = "Nome";
+            dados.Columns["telefone_con"].ColumnName = "Telefone";
+            dados.Columns["email_con"].ColumnName = "Email";
+            dados.Columns["data_con"].ColumnName = "Data";
+            dados.Columns["sexo_con"].ColumnName = "Sexo";
+        }
+
+        private void btPesquisar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpesquisar_TextChanged(object sender, EventArgs e)
+        {
+            var nome = txtpesquisar.Text;
+            var sql = $"select * from contato_con where nome_con like '{nome}%';";
+            var cmd = new MySqlCommand(sql, conexao);
+            var da = new MySqlDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+            DataTable dados = new DataTable();
+            da.Fill(dados);
+            dgvListar.DataSource = dados;
+            dados.Columns["id_con"].ColumnName = "id";
+            dados.Columns["nome_con"].ColumnName = "Nome";
+            dados.Columns["telefone_con"].ColumnName = "Telefone";
+            dados.Columns["email_con"].ColumnName = "Email";
+            dados.Columns["data_con"].ColumnName = "Data";
+            dados.Columns["sexo_con"].ColumnName = "Sexo";
+        }
+    }
+}
